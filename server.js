c70f520096384254a5224aee447c7b06696930bc
Vivek <vivekprasad1410@gmail.com>
2020-06-28 20:59:12 +0530

    modifying for react start

---
+++ server.js
@@ -0,0 +1,35 @@
+import * as functions from 'firebase-functions';
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import App from './src/app';
+import html from './src/html';
+import { ServerStyleSheet } from 'styled-components';
+
+const port = 3000;
+const app = express();
+
+const ENVIRONMENT = process.env.NODE_ENV;
+
+const staticServe = ENVIRONMENT === 'production' ? express.static("src") : express.static("dist/src"); 
+
+app.use(staticServe);
+
+app.get('**', (req, res) => {
+    const sheet = new ServerStyleSheet();
+    const body = renderToString(sheet.collectStyles(<App />));
+    const styles = sheet.getStyleTags();
+    const title = 'react ssr';
+    res.send(
+        html({
+            body,
+            styles,
+            title,
+        })
+    );
+});
+
+//for firebase functions - hosting
+// export let ssrapp = functions.https.onRequest(app);
+
+app.listen(port, () => console.log('server is listening'));
---
+++ src/global.js
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+*,
+*:before,
+*:after {
+    box-sizing: border-box;
+}
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0px;
+    padding: 0px;
+    height: 100vh;
+    font-family: 'Courier Prime', monospace;
+    background-color: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text};
+    transition: all 0.25s linear;
+}
+button {
+    padding: 10px;
+    color: darkgreen;
+    border-radius: 5px;
+    border: 2px solid darkgreen;
+    font-family: 'Courier Prime', monospace;
+    cursor: pointer;
+}
+button:hover {
+    color: #fff;
+    background-color: darkgreen;
+}
+button:focus {
+    outline: none;
+}
+`;
