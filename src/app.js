102c2a8891619df842a5ff2dc55a4f14a0898da6
Vivek <vivekprasad1410@gmail.com>
2020-07-01 10:52:05 +0530

    added toggle button functionality

--- src/app.js
+++ src/app.js
@@ -1,11 +1,59 @@
 import React, { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { GlobalStyle } from './global';
 import { lightTheme, darkTheme } from './theme';
 import sun from './sun.png';
 import moon from './moon.png';
+
+const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100px;
+    width: 100%;
+    padding: 10px 50px;
+`;
+const ToggleDiv = styled.div`
+    height: 27px;
+    width: 50px;
+    position: relative;
+    border-radius: 30px;
+    border: 2px solid ${(props) => props.theme.primaryColor};
+    background-color: ${(props) => props.theme.background};
+    transition: all 0.25s linear;
+`;
+const ImageSun = styled.img`
+    position: absolute;
+    height: 16px;
+    width: 16px;
+    left: 3px;
+    top: 3px;
+`;
+const ImageMoon = styled.img`
+    position: absolute;
+    height: 16px;
+    width: 16px;
+    right: 3px;
+    top: 3px;
+`;
+const ToggleThumb = styled.div`
+    height: 27px;
+    width: 27px;
+    position: absolute;
+    left: -2px;
+    top: -2px;
+    border-radius: 50%;
+    background-color: ${(props) => props.theme.primaryColor};
+    transform: translateX(0);
+    transition: transform 0.25s linear;
+    ${(props) =>
+        props.themeState === 'dark' &&
+        css`
+            transform: translateX(23px);
+        `}
+`;
 
 function App() {
     const [theme, setTheme] = useState('light');
@@ -16,48 +64,6 @@ function App() {
             setTheme('light');
         }
     };
-    const Header = styled.header`
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        height: 100px;
-        width: 100%;
-        padding: 10px 50px;
-    `;
-    const ToggleDiv = styled.div`
-        height: 27px;
-        width: 50px;
-        position: relative;
-        border-radius: 30px;
-        border: 2px solid ${props => props.theme.primaryColor};
-        background-color: ${props => props.theme.contrastBg};
-    `;
-    const ImageSun = styled.img`
-        position: absolute;
-        height: 16px;
-        width: 16px;
-        left: 3px;
-        top: 3px;
-    `;
-    const ImageMoon = styled.img`
-        position: absolute;
-        height: 16px;
-        width: 16px;
-        right: 3px;
-        top: 3px;
-    `;
-    const ToggleThumb = styled.div`
-        height: 27px;
-        width: 27px;
-        position: absolute;
-        left: -2px;
-        top: -2px;
-        border-radius: 50%;
-        border: 2px solid ${props => props.theme.primaryColor};
-        background-color: ${props => props.theme.background};
-        transform: ${theme === 'light' ? 'translateX(0)' : 'translateX(20px)' };
-        transition: all 0.5s ease-in-out;
-    `;
     return (
         <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
             <GlobalStyle />
@@ -66,7 +72,7 @@ function App() {
                 <ToggleDiv onClick={toggleTheme}>
                     <ImageSun src={sun} alt='' />
                     <ImageMoon src={moon} alt='' />
-                    <ToggleThumb />
+                    <ToggleThumb themeState={theme} />
                 </ToggleDiv>
             </Header>
             <button>toggle theme</button>
--- src/theme.js
+++ src/theme.js
@@ -2,12 +2,14 @@ export const lightTheme = {
     background: '#E2E2E2',
     contrastBg: '#363537',
     primaryText: '#363537',
-    primaryColor: '#006400'
+    primaryColor: '#0044a2',
+    secondaryColor: '#5ba0ff'
 };
 
 export const darkTheme = {
     contrastBg: '#E2E2E2',
     background: '#363537',
     primaryText: '#FAFAFA',
-    primaryColor: '#006400'
+    primaryColor: '#5ba0ff',
+    secondaryColor: '#0044a2'
 };
