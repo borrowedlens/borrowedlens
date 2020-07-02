a89599fe1fb2f59a48d0528a77b490e205dae4c2
Vivek <vivekprasad1410@gmail.com>
2020-07-02 20:55:23 +0530

    export images as array

---
+++ src/images.js
@@ -0,0 +1,15 @@
+import photo1 from './assets/images/1.jpg';
+import photo2 from './assets/images/2.jpg';
+import photo3 from './assets/images/3.jpg';
+import photo4 from './assets/images/4.jpg';
+import photo5 from './assets/images/5.jpg';
+import photo6 from './assets/images/5.jpg';
+
+export default [
+    photo1,
+    photo2,
+    photo3,
+    photo4,
+    photo5,
+    photo6
+]
