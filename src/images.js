3d55de0e83e517291cf759c0f6714ff02109a52e
Vivek <vivekprasad1410@gmail.com>
2020-07-02 22:56:26 +0530

    adding image6

--- src/images.js
+++ src/images.js
@@ -3,7 +3,7 @@ import photo2 from './assets/images/2.jpg';
 import photo3 from './assets/images/3.jpg';
 import photo4 from './assets/images/4.jpg';
 import photo5 from './assets/images/5.jpg';
-import photo6 from './assets/images/5.jpg';
+import photo6 from './assets/images/6.jpg';
 
 export default [
     photo1,
